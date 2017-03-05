3761cde7e52d4800bd58c17c0f523cb59e2caa87
spShashankGit <shashank_pandey93@yahoo.com>
2017-03-05 15:37:43 +0530

    server.js
    server

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
 var app = express();
 app.use(morgan('combined'));
-
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/ui/reg', function (req, res) {
+    res.send('This is a response for reg pagw!');
+});
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+    res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+    res.sendFile(path.join(__dirname, 'ui', 'mad.png'));
 });
-
-
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+    console.log(`IMAD course app listening on port ${port}!`);
 });
+s
